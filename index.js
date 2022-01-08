d63241c5c2a634cf694a0f8b59f1a87855b2b7fd
Ananthakrishnan S <ananthakrishnans.muhamma@gmail.com>
2022-01-08 19:18:23 +0530

    added views folder, required mongoose, db connected

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const port = 3000
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/test')
+.then(()=>{
+    console.log('database connection established' )
+})
+.catch(err => {
+    console.log('error while connecting to database')
+    console.log(err)
+})
 
 app.set('views',path.join(__dirname,'views'))
 app.set('view engine','ejs')
